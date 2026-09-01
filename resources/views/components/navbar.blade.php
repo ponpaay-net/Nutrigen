a4b5996fa4d0493db4716c0b86dff7dddccef23f
Ponpay <muhamadnoval983@gmail.com>
2026-09-01 22:35:11 +0700

    fix(kader): notifikasi modal - ganti x-cloak > style display none (hindari terkunci di x-teleport)

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -89,7 +89,7 @@
 
         <!-- Notification Modal (teleported to body, shares openNotif scope) -->
         <div x-teleport="body">
-            <div x-show="openNotif" x-cloak class="fixed inset-0 z-[140] flex p-3 sm:p-4">
+            <div x-show="openNotif" style="display: none;" class="fixed inset-0 z-[140] flex p-3 sm:p-4">
                 <div class="absolute inset-0 bg-slate-900/50 backdrop-blur-sm" @click="openNotif = false"></div>
                 <div class="relative w-full max-w-lg mx-auto sm:my-auto bg-white rounded-2xl shadow-2xl border border-slate-100 overflow-hidden flex flex-col"
                      x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95 translate-y-2" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95">
